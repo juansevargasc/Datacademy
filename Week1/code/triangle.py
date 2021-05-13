f20c6faed601c1f4d4116fabae61a9f6fbdcad5c
Juanse <jsvargasc@unal.edu.co>
2021-05-13 10:49:46 -0500

    triangle modification 1

--- Week1/code/triangle.py
+++ Week1/code/triangle.py
@@ -1,8 +1,19 @@
+import math
+
 def calculate_area( h, b ):
     return (h * b) / 2
 
 def triangle_class(h, b):
-    return None
+    '''
+    Sí es triángulo equilátero, h = sqrt(3) / 2 * b.
+    Si es triángulo equilátero
+    '''
+    if h == ( (math.sqrt(3) / 2) * b ):
+        return 'Equilátero'
+    if h == ( math.sqrt(3) * b ):
+        return 'Isósceles'
+    else:
+        return 'Escaleno'
 
 
 if __name__ == '__main__':
@@ -11,4 +22,7 @@ if __name__ == '__main__':
 
     area = calculate_area(base, altura)
     print(f'Area: {area}')
+
+    clasificacion = triangle_class(base, altura)
+    print(f'Clasificación: {clasificacion}')
     
